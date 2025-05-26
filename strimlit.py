a6ff889fdce6d2b949ce1249e236e974b1175675
adrian531 <68359345+adrian531@users.noreply.github.com>
2025-05-26 23:46:12 +0700

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -96,9 +96,8 @@ with st.form(key='prediction_form'):
             if col.lower() == 'oldpeak':
                 user_input[col] = st.number_input(f"📉 {col}", value=0.0, step=0.1, format="%.1f")
             else:
-                user_input[col] = st.number_input(
-                "📊 Umur (contoh: 19 tahun)", value=0, step=1, format="%d"
-                )
+                label = f"📊 {col} (contoh: 19)" if col.lower() == "age" else f"📊 {col}"
+                user_input[col] = st.number_input(label, value=0, step=1, format="%d")
 
 
     submit_button = st.form_submit_button(label='🔍 Prediksi')
